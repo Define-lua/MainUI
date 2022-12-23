5b0a1a3cbc28460cc0ac5cb8916c1bdb6d53cb4d
Define-lua <110200945+Define-lua@users.noreply.github.com>
2022-12-23 02:43:40 +0000

    Update .lua

--- .lua
+++ .lua
@@ -20,7 +20,7 @@ local library = {
     ended = {},
     changed = {},
     folders = {
-        assets = "Defineware.lua/assets",
+        assets = "Lynx.Private/assets",
     },
     shared = {
         initialized = false,
@@ -393,8 +393,8 @@ do
     --
     function library:New(info)
 	local info = info or {}
-        local name = info.name or info.Name or "Defineware.lua"
-        local title = info.title or info.Title or "Defineware.lua"
+        local name = info.name or info.Name or "Lynx.Private"
+        local title = info.title or info.Title or "Lynx.Private"
         local size = info.size or info.Size or Vector2.new(504,604)
         local accent = info.accent or info.Accent or info.color or info.Color or theme.accent
         --
@@ -783,7 +783,7 @@ do
             --
             spawn(function()
                 while wait(0.1) do
-                    watermark_title.Text = string.format("Defineware.lua | Private Build | Ping : " .. tostring(library.shared.ping) .. "ms | FPS : " .. tostring(library.shared.fps), window.name)
+                    watermark_title.Text = string.format("Lynx | Private Build | Ping : " .. tostring(library.shared.ping) .. "ms | FPS : " .. tostring(library.shared.fps), window.name)
                     window.watermark:UpdateSize()
                 end
             end)
